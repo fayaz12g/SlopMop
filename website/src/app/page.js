8d50aee455c6006a29b4d4d42a126a9242774e52
Fayaz Shaikh <61674751+fayaz12g@users.noreply.github.com>
2026-02-14 11:42:32 -0500

    Create simple routing

--- website/src/app/page.js
+++ website/src/app/page.js
@@ -1,6 +1,11 @@
+"use client";
 import Image from "next/image";
 
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter();
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -35,10 +40,10 @@ export default function Home() {
           </p>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
+          <button
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
+            onClick={() => router.push("/install")}
             rel="noopener noreferrer"
           >
             <Image
@@ -49,7 +54,7 @@ export default function Home() {
               height={16}
             />
             Deploy Now
-          </a>
+          </button>
           <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
